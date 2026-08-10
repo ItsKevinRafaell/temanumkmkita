62038b7b0c6a254ae43a78dcd9324e56099f3a8c
ItsKevinRafaell <itskevin.rafael@gmail.com>
2026-08-10 17:32:07 +0700

    feat: rombak 3 demo porto (garasi/rania/rupa) DNA unik + foto UGC + noindex porto

--- src/app/robots.ts
+++ src/app/robots.ts
@@ -7,7 +7,7 @@ export default function robots(): MetadataRoute.Robots {
       {
         userAgent: "*",
         allow: "/",
-        disallow: ["/admin/", "/api/", "/preview/"],
+        disallow: ["/admin/", "/api/", "/preview/", "/porto/"],
       },
     ],
     sitemap: `${SITE_URL}/sitemap.xml`,
